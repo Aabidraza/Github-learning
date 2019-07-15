10ebac6aacbfd03f22f70a9881072e7310d90448
Rajeev <abid786raza@gmail.com>
2019-07-15 15:22:49 +0530

    changed

--- ar.php
+++ ar.php
@@ -3,15 +3,7 @@ $a=array('5'=>2,'8'=>4,'9'=>3);
 $b=array('Parent'=>'5','Parent'=>'8','Parent'=>'9');
 $f = array_flip($b);
 
-//$c = array_intersect_key($a,$f);
 
-	/*foreach ($a as $key => $value) {
-
-		foreach ($b as $key => $value) {
-	
-		}
-
-	}*/
 				$c = array_map(function($b) use ($a){
 						$chng = $a[$b['Parent']];
 						$b['Parent'] = $chng;
